4dd259f08548b75d83f90c45721211e801b3e8ba
Zach Tollefson <tolle239@umn.edu>
2020-10-27 10:50:04 -0500

    add github pages functionality

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -10,10 +10,12 @@ const About = () => {
             <div className="profile">
                 <img src={profile} className="profile-pic" alt="Me" />
                 <div className="summary">
+                    <h4>Are you hirable?</h4>
+                    <p>Yes, I am looking for a full-time developer opportunity. </p>
                     <h4>What is my approach to web development?</h4>
-                    <p>I prefer to create simple and responsive sites that are easy to navigate over those that are overly complex. However, I still strive for a unique user experience with elegant design and engaging functionality.</p>
+                    <p>I prefer to create simple and responsive sites that are easy to navigate over those that are overly complex. However, I still strive for a unique user experience with elegant design and engaging functionality. I mainly use the MERN stack to create stuff, but am open to learning new technologies if the problem calls for it. </p>
                     <h4>Why am I drawn to this field?</h4>
-                    <p> I, both the creativity that comes from problem-solving and those of implementing a design. </p>
+                    <p> I enjoy the creativity that comes from problem-solving and implementing a design. I enjoy creating practical solutions to problems and sharing them with my friends!</p>
                     <h4>How did I get to this point in my career?</h4>
                     <p> While I've written code since I was in high school, I began my career as a chemical engineer. In my first role, I worked on a team of software engineers to develop process design software for the oil and gas industry. Enamoured with the software development process, I started to study web development in my free time with online courses and transitioned to the technology industry.</p>
                 </div>
@@ -24,15 +26,18 @@ const About = () => {
                     <ul className="skills">
                         <li>HTML</li>
                         <li>CSS</li>
-                        <li>Javascript</li>                       
+                        <li>SASS</li>
+                        <li>Javascript</li> 
+                        <li>Typescript</li>           
                         <li>React (with Hooks)</li>
                     </ul>
                 </div>
                 <div className="skill-area">
                     <h4>Back-End</h4>
                     <ul className="skills">
-                        <li>ASP .NET</li>
-                        <li>Postgres</li>                       
+                        <li>Node.js</li>
+                        <li>Express</li>
+                        <li>PostgreSQL</li>                       
                         <li>MongoDB</li>
                     </ul>
                 </div>
@@ -40,7 +45,7 @@ const About = () => {
                     <h4>Other</h4>
                     <ul className="skills">
                         <li>Git</li>
-                        <li>GitHub</li>
+                        <li>Jest (testing framework)</li>
                     </ul>
             </div>
             </div>
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -12,33 +12,41 @@ const Projects = () => {
                     <Project  
                     title="Kanban Board"  
                     description="An agile project management tool for tracking bugs, enhancements, and other tasks in a project pipeline."
-                    skills="React, Bootstrap"/>
+                    skills="React, Bootstrap, Node.js, SQL"
+                    githublink="www.github.com/ztollef/kanban-board"
+                    livesite="www.zachtollefson.com"
+                    />
                 </div>
                 <div className="project">
                     <Project className="project"
                     title="COVID-19 Tracker"  
                     description="Uses data from Johns Hopkins to visualize COVID-19 cases across the world."
-                    skills="Javascript, Open Source API, JSON, SASS"
+                    skills="Typescript, SASS"
+                    githublink="www.github.com/ztollef/covid-tracker"
+                    livesite="www.zachtollefson.com"
                     />
                 </div>
                 <div className="project">
                     <Project className="project"
                     title="Fido Fetch API"  
                     description="An open source RESTful API where users can pull information about different dog breeds, including temperament, life expectancy, and weight."
-                    skills="REST API, C# .NET, MySQL"
+                    skills="REST API, Node.js, Express, MySQL"
                     />
                 </div>
                 <div className="project">
                     <Project className="project"
                     title="Vintage Videogame E-Commerce"  
-                    description="Mock e-commerce site for purchasing vintage videogames. Complete with a front-end, back-end, and connection to database. Features include authentication, connection to Paypal API, security."
-                    skills="React, Redux, CSS, C#, MySQL" />
+                    description="Mock e-commerce site for finding and purchasing vintage videogames. Complete with a front-end, back-end, and connection to database. Features include authentication, connection to Paypal API, security."
+                    skills="React, Redux, Node.js, Express, JWT Authentication, Single Page Application, MySQL" />
                 </div>
                 <div className="project">
                     <Project className="project"
                     title="Portfolio Site"  
                     description="My personal portfolio site. You're on it now!"
-                    skills="React, Typescript, SASS" />
+                    skills="React, Typescript, SASS"
+                    githublink="www.github.com/ztollef/covid-tracker"
+                    livesite="www.zachtollefson.com"
+                     />
                 </div>
             </div>
             
