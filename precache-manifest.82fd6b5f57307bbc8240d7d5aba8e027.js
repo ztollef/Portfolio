ad92dcba79c0535b16fff445d891d72a8917f9e5
Zach Tollefson <tolle239@umn.edu>
2020-10-28 23:16:17 -0500

    Updates

---
+++ precache-manifest.82fd6b5f57307bbc8240d7d5aba8e027.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04925c420e485f15e4511278adac7b96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c7c4fe08570ecb87594",
+    "url": "/static/css/main.ab92ecf7.chunk.css"
+  },
+  {
+    "revision": "afd6b6ed0e121ff51650",
+    "url": "/static/js/2.e99cc1e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e99cc1e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c7c4fe08570ecb87594",
+    "url": "/static/js/main.68a7c47a.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "0dece6d6f7291a1eea28b4777a8c04c2",
+    "url": "/static/media/LI-In-Bug.0dece6d6.png"
+  },
+  {
+    "revision": "59b579b37a5d473f3434162c88ddf4cc",
+    "url": "/static/media/profile-picture.59b579b3.jpg"
+  }
+]);
